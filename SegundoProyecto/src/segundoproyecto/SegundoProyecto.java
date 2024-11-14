15dc935b6944f398a831591c7c001c0f73c5ee22
Nicolás González Dobarro <nicolasgondob@gmail.com>
2024-11-14 09:41:32 +0100

    Add files via upload

---
+++ SegundoProyecto/src/segundoproyecto/SegundoProyecto.java
@@ -0,0 +1,16 @@
+/*
+
+ */package segundoproyecto;
+
+/**
+ *
+ * @author Nicolás González Dobarro
+ */
+public class SegundoProyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+}
---
+++ TiposdeDatos/src/tiposdedatos/TiposdeDatos.java
@@ -0,0 +1,41 @@
+/*
+
+ */
+package tiposdedatos;
+
+/**
+ *
+ * @author Nicolás González Dobarro
+ */
+public class TiposdeDatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int ad = 1_000_000, a = 20;
+        short b = 20;
+        byte c = 127;
+        long d = 37_789_535;
+
+        float e = 5.5f;
+        double g = 5.5;
+        double h = 1.5;
+
+        {/* Character */
+            char j = 65;
+            char k = 66;
+
+            System.out.println("");
+        }
+
+        {/* Booleanos */
+            boolean cierto = true;
+            boolean falso = false;
+        }
+
+        System.out.println(10 / 3);
+
+    }
+
+}
